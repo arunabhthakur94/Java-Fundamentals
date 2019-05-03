8cdbd540009063f28ba7d41e98a14bb3399528c9
Lenovo <Lenovo@192.168.1.2>
2019-05-03 11:26:39 +0530

    Employee

--- Fundamentals/src/com/srmicro/annotations/Lab731.java
+++ Fundamentals/src/com/srmicro/annotations/Lab731.java
@@ -1,1 +1,22 @@
+package com.srmicro.annotations;
 
+public class Lab731 {
+	public static void main(String[] args) {
+		Employee e1 = new Employee(101, "AAA");
+		System.out.println(e1);
+	}
+}
+
+class Employee {
+	int eid;
+	String ename;
+
+	Employee(int eid, String ename) {
+		this.eid = eid;
+		this.ename = ename;
+	}
+
+	public String toString() {
+		return eid + "\t" + ename;
+	}
+}
